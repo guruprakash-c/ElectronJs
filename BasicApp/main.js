41f5f24b7a7bd96b58d43ffaafe97507ad477a90
Guru Prakash <10187004+guruprakash-c@users.noreply.github.com>
2025-10-25 18:41:22 +0530

    Integrated Bootstrap

--- BasicApp/main.js
+++ BasicApp/main.js
@@ -1,50 +1,94 @@
-// In the main process.
-const { electron, BrowserWindow } = require('electron') 
-const app = electron.BrowserWindow 
-const path = require('path')
+import { app, BrowserWindow, ipcMain, nativeTheme } from 'electron'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import url from 'url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url)); 
-
-let options = {
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+var options = {
     width: 800,
     height: 600,
     icon: path.join(__dirname, 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    },
-    // show: false
+    }
 };
-function createWindow (Window, options, url, isDevTools, isUrlExternal) {
-    Window = new BrowserWindow(options);
+var xUrl = 'https://www.devstoc.com';
+var isUrlExternal = false;
+var isDevTools = false;
 
-    if (isUrlExternal) {
-        Window.loadURL(url);
-    } else {
-        Window.loadFile(url.format({
-            pathname: path.join(__dirname, 'index.html'),
-            protocol: 'file:',
-            slashes: true
-        }));
-    }
-    if (isDevTools) {
-        Window.webContents.openDevTools();
-    }
-    window.once('ready-to-show', () => {
-        window.show();
-    });
+
+function createWindow () {
+const parentWndw = new BrowserWindow({
+  width: 800,
+  height: 600,
+  title: "Parent Window",
+  icon: path.join(__dirname, 'icon.png'),
+  webPreferences: {
+    preload: path.join(__dirname, 'preload.js')
+  },
+  // backgroundColor: '#efefef',
+  // minimizable: false,
+  // maximizable: true,
+  // autoHideMenuBar: true,
+  // fullscreenable: false,
+  // resizable: false,
+  // modal: true,
+})
+// parentWndw.webContents.openDevTools();
+parentWndw.loadURL(url.format({
+  pathname: path.join(__dirname, 'index.html'),
+  protocol: 'file:',
+  slashes: true
+}));
+// const childWndw = new BrowserWindow({
+//   show: false,
+//   title: "Child Window",
+//   width: 700,
+//   height: 450,
+//   icon: path.join(__dirname, 'icon.png'),
+//   webPreferences: {
+//     preload: path.join(__dirname, 'preload.js')
+//   },
+//   parent: parentWndw,
+//   modal: true,
+//   minimizable: false,
+//   maximizable: false,
+// });
+// //childWndw.webContents.openDevTools();
+// childWndw.loadURL(xUrl);
+// childWndw.once('ready-to-show', () => {
+//   childWndw.show();
+// });
 }
 
-const win = new BrowserWindow();
+ipcMain.handle('dark-mode:toggle', () => {
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light'
+  } else {
+    nativeTheme.themeSource = 'dark'
+  }
+  return nativeTheme.shouldUseDarkColors
+})
+
+ipcMain.handle('dark-mode:system', () => {
+  nativeTheme.themeSource = 'system'
+})
+
+// const view = new BrowserView();
+//   win.setBrowserView(view);
 
 app.whenReady().then(() => {
-  createWindow(win, options, '', false, false);
+  createWindow()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow(win, options, '', false, false)
+      createWindow()
     }
   })
-});
+})
+
+// app.commandLine.appendSwitch('disable-features', 'crashpad');
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
